d727f94433124e5c9019a8254e511989626ce559
wmuruet <schrodinger.platypus@gmail.com>
2023-05-15 14:12:11 +0100

    - Initial commit install_required_packages.R -- Started R script

---
+++ Src/R/install_required_packages.R
@@ -0,0 +1,10 @@
+# A function to install all or some packages required by the project
+
+install_required_packages <- function() {
+  required_packages_list <- list(
+    data_info <- c("readr", "magrittr")
+  )
+}
+
+install_required_packages()
+rm(install_required_packages)
