8a97b80cd9e90219055af898198019e2312d09d8
wmuruet <schrodinger.platypus@gmail.com>
2023-05-15 14:33:57 +0100

    - Functionality added -- instal_required_packages now actually works
    - Required package added
    -- dplyr was added to the list of required packages

--- Src/R/install_required_packages.R
+++ Src/R/install_required_packages.R
@@ -1,9 +1,22 @@
 # A function to install all or some packages required by the project
 
 install_required_packages <- function() {
-  required_packages_list <- list(
-    data_info <- c("readr", "magrittr")
-  )
+  required_packages <- 
+    c(
+      #Data Loading
+      "readr",
+      
+      #Data manipulation
+      "dplyr",
+      
+      #Utilities
+      "magrittr")
+  
+  to_install <- setdiff(required_packages, installed.packages())
+  
+  if (length(to_install) > 0) install.packages(to_install, 
+                                               dependencies = TRUE,
+                                               quiet = TRUE)
 }
 
 install_required_packages()
